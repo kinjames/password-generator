fd3132a89268712c85d4c55d3fb388b58a0e4874
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-04-06 21:26:11 +0000

    Add files via upload

--- apps/js/script.js
+++ apps/js/script.js
@@ -1,14 +1,22 @@
 let slider = document.getElementById('myRange');
 let output = document.querySelector('.password-number');
 let passText = document.querySelector('.passwordText').innerHTML;
-let clipBoard = document.querySelector('.clip')
+let clipBoard = document.querySelector('.clip');
+let btn = document.getElementById('btn');
+let btns = document.querySelectorAll('.btns');
+
 
 output.innerHTML = slider.value;
+
+btn.onsubmit = () =>{
+    generatePassword();
+}
 
 slider.oninput = () =>{
     output.innerHTML = slider.value;
     handleInputChange();
 }
+
 
 clipBoard.onclick = () =>{
     copyToClipboard();
@@ -32,6 +40,42 @@ const showPopUp = () =>{
         popup.style.display = 'block';
 
 }
+
+const generatePassword = (length,hasNumber,hasUppercase,hasLowercase,hasSymbol) =>{
+    let passWord = '';
+    let characters = '';
+
+    if(hasNumber){
+        characters += '0123456789';
+    }
+    if(hasUppercase){
+        characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if(hasLowercase){
+        characters += 'abcdefghijklmnopqrstuvwxyz';
+    }
+    if(hasSymbol){
+        characters += '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    }
+
+    for (let i = 0; i< length; i++){
+        let ranNum = Math.floor(Math.random()*characters.length);
+        passWord += characters[ranNum]
+    }
+
+    return passWord;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
